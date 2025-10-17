38c07dd1f586e400a63208fc92953b839bda831f
MUHAMMED ALTHAF O K <althff10@gmail.com>
2025-10-17 11:41:22 +0530

    Update 10.Student_dict.py

--- 10.Student_dict.py
+++ 10.Student_dict.py
@@ -1,23 +1,23 @@
 student = {
-    "name": "Nihal",
-    "roll_number": 37,
-    "register_number": "19857",
+    "name": "Althaf",
+    "roll_number": 42,
+    "register_number": "19799",
     "department": "Computer Applications",
     "semester": 1
 }
 
-student["total_mark"] = 85
+student["total_mark"] = int(input("Enter the total Marks :"))
 
 def grade(total_mark):
     if total_mark >= 90:
         return 'A'
-    elif total_mark >= 82:
+    elif total_mark >= 85:
         return 'B'
-    elif total_mark >= 75:
+    elif total_mark >= 80:
         return 'C'
-    elif total_mark >= 60:
+    elif total_mark >= 75:
         return 'D'
-    elif total_mark >= 50:
+    elif total_mark >= 60:
         return 'P'
     else:
         return 'F'
@@ -27,3 +27,4 @@ student["grade"] = grade(student["total_mark"])
 del student["roll_number"]
 
 print(student)
+
